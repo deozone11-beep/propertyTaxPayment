0da6309a4198f0615c9ccfcabef994213ef95304
deozone11-beep <deozone11@gmail.com>
2026-06-04 23:30:19 +0530

    updated on 04.06.26

--- src/components/QrCode.jsx
+++ src/components/QrCode.jsx
@@ -1,61 +1,6 @@
 import { useState } from "react"
 
 
-// export const QrCode = () => {
-
-//   const [img, setImg] = useState("");
-//   const [loading, setLoading] = useState(false);
-//   const [qrData, setQrData] = useState("00-000-00000-000");
-//   const [qrSize, setQrSize] = useState("150");
-
-
-//   async function generateQR(){
-//     setLoading(true);
-//     try{
-//       const url = `https://api.qrserver.com/v1/create-qr-code/?size=${qrSize}x${qrSize}&data=${encodeURIComponent(qrData)}`;
-//       setImg(url);
-//     }catch (error){
-//       console.error("Error generating QR Code", error);
-//     }finally {
-//       setLoading(false)
-//     }
-//   }
-
-//   function downloadQR(){
-//     fetch(img)
-//     .then ((response)=> response.blob())
-//     .then ((blob)=>{
-//       const link = document.createElement("a");
-//       link.href = URL.createObjectURL(blob);
-//       link.download = `${qrData}.png`;
-//       document.body.appendChild(link);
-//       link.click();
-//       document.body.removeChild(link);
-//     })
-//   }
-
-
-//   return (
-//     <div className="app-container">
-//         <h1>QR CODE GENERATOR</h1>
-//         {loading && <p>Please wait...</p>}
-//         {img && <img src={img} className="qr-code-image"></img>}
-        
-//         <div>
-//             <label htmlFor="dataInput" className="input-label">Data for QR Code:</label>
-//             <input type="text" value={qrData} id="dataInput" placeholder="Enter data for QR Code" onChange={(e)=>setQrData(e.target.value)} />
-//             <label htmlFor="sizeInput" className="input-label">Image Size (e.g., 150)</label>
-//             <input type="text" value={qrSize} id="sizeInput" placeholder="Enter Image size" onChange={(e)=>setQrSize(e.target.value)}/>
-//             <button className="generate-button" disabled={loading} onClick={generateQR}>Generate QR Code</button>
-//             <button className="download-button" onClick={downloadQR}>Download QR Code</button>
-//         </div>
-//         <p className="footer">Designed By <a href="#">Preetha Kumar</a></p>
-//     </div>
-//   )
-// }
-
-
-
 export const QrCode = () => {
   const [value, setValue] = useState("");
   const [img, setImg] = useState("");
